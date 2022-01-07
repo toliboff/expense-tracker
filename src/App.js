ae5f18a6f3faf07557e241d04000ea766e79ee2c
Tolibjon <tolibjohn@gmail.com>
2022-01-07 22:15:35 +0500

    Create components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import Balance from './components/Balance';
+import Header from './components/Header';
+import IncomeExpenses from './components/IncomeExpenses';
 
 function App() {
   return (
     <div>
-     Hello World
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+      </div>
     </div>
   );
 }
---
+++ src/components/TransactionList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const TransactionList = () => {
+  return (
+    <>
+      <h3>History</h3>
+      <ul></ul>
+      
+    </>
+  )
+}
+
+export default TransactionList
