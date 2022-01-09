1cc254322ca722f9aa32c10b9d8c999bc5a24557
Tolibjon <tolibjohn@gmail.com>
2022-01-09 22:21:29 +0500

    Create dummy data

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import Transaction from './Transaction'
 
 const TransactionList = () => {
+  const transactions=[
+    { id: 1, title: 'Transaction 1', amount: 50 },
+    { id: 2, title: 'Transaction 2', amount: 75 },
+    { id: 3, title: 'Transaction 3', amount: 95 },
+  ]
   return (
     <>
       <h3>History</h3>
-      <ul></ul>
+      {transactions.map((transaction)=><Transaction transaction = {transaction}/>)}
       
     </>
   )
